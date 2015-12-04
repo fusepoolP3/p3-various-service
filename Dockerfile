43401b024a73e6bb7aed08f1885b349f61a30c0c
Rupert Westenthaler <rupert.westenthaler@gmail.com>
2015-12-04 08:23:46 +0100

    FP-379: registered the new Literal Extraction Transformer Factory GUI with the various services module

--- Dockerfile
+++ Dockerfile
@@ -31,6 +31,7 @@ RUN cd /usr/local/lib/ && \
     curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-xslt-transformer/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-xslt-transformer.jar && \
     curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-dictionary-matcher-transformer/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-dictionary-matcher-transformer.jar && \
     curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-literal-extraction-transformer/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-literal-extraction-transformer.jar && \
+    curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-literal-extraction-factory-gui/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-literal-extraction-factory-gui.jar && \
     curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-geocoordinates-transformer/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-geocoordinates-transformer.jar && \
     curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-dashboard/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-dashboard.jar && \
     curl -Ls $(curl -s https://api.github.com/repos/fusepoolP3/p3-pipeline-gui/releases | grep browser_download_url | head -n 1 | cut -d '"' -f 4) > p3-pipeline-gui.jar && \
--- startup.sh
+++ startup.sh
@@ -20,27 +20,28 @@ fi
 
 echo LDPURI: $LDPURI
 
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-proxy.jar -T $LDPURI > /var/log/p3-proxy.log 2>&1 &    	    # Port 8181
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-proxy.jar -T $LDPURI > /var/log/p3-proxy.log 2>&1 &    	                                # Port 8181
 # Others
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-transformer-web-client.jar > /var/log/web-client.log 2>&1 &    # Port 8151
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-transformer-web-client.jar > /var/log/web-client.log 2>&1 &                              # Port 8151
 
 # GUIs
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-dashboard.jar  &                               # Port 8200
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-pipeline-gui.jar &                             # Port 8201
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-dictionary-matcher-factory-gui.jar &           # Port 8202
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-batchrefine-factory-gui.jar -P 8203 &
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-xslt-factory-gui.jar &                         # Port 8204
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-resource-gui.jar &                             # Port 8205
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-dashboard.jar  > /var/log/dashboard-gui.log 2>&1 &                                       # Port 8200
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-pipeline-gui.jar > /var/log/pipeline-gui.log 2>&1 &                                      # Port 8201
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-dictionary-matcher-factory-gui.jar > /var/log/dictionary-matcher-factory-gui.log 2>&1 &  # Port 8202
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-batchrefine-factory-gui.jar -P 8203 > /var/log/batchrefine-factory-gui-gui.log 2>&1 &    # Port 8203
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-xslt-factory-gui.jar > /var/log/xslt-factory-gui.log 2>&1 &                              # Port 8204
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-resource-gui.jar > /var/log/resource-gui.log 2>&1 &                                      # Port 8205
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-literal-extraction-factory-gui.jar > /var/log/literal-extraction-factory-gui.log 2>&1 &  # Port 8206
 
 # Transformers
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-pipeline-transformer.jar -C > /var/log/pipeline-transformer.log 2>&1 &              # Port 8300
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-dictionary-matcher-transformer.jar > /var/log/dictionary-matcher.log 2>&1 &         # Port 8301
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-geo-enriching-transformer.jar -P 8302  > /var/log/geo-enriching.log 2>&1  & 
-su p3 -s /usr/bin/java -- -Xmx1g -jar /usr/local/lib/p3-any23-transformer.jar > /var/log/any23-transformer.log 2>&1 &                # Port 8303
-su p3 -s /usr/bin/java -- -Xmx2g -jar /usr/local/lib/p3-literal-extraction-transformer.jar > /var/log/literal-extraction.log 2>&1 &  # Port 8305
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-silkdedup.jar > /var/log/silkdedup.log 2>&1 &                                       # Port 8306
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-xslt-transformer.jar > /var/log/xslt-transformer.log 2>&1 &                         # port 8307
-su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-geocoordinates-transformer.jar -P 8308 > /var/log/geocoordinates-transformer.log 2>&1 &
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-pipeline-transformer.jar -C > /var/log/pipeline-transformer.log 2>&1 &                   # Port 8300
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-dictionary-matcher-transformer.jar > /var/log/dictionary-matcher.log 2>&1 &              # Port 8301
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-geo-enriching-transformer.jar -P 8302  > /var/log/geo-enriching.log 2>&1  &              # Port 8302
+su p3 -s /usr/bin/java -- -Xmx1g -jar /usr/local/lib/p3-any23-transformer.jar > /var/log/any23-transformer.log 2>&1 &                     # Port 8303
+su p3 -s /usr/bin/java -- -Xmx4g -jar /usr/local/lib/p3-literal-extraction-transformer.jar > /var/log/literal-extraction.log 2>&1 &       # Port 8305
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-silkdedup.jar > /var/log/silkdedup.log 2>&1 &                                            # Port 8306
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-xslt-transformer.jar > /var/log/xslt-transformer.log 2>&1 &                              # port 8307
+su p3 -s /usr/bin/java -- -jar /usr/local/lib/p3-geocoordinates-transformer.jar -P 8308 > /var/log/geocoordinates-transformer.log 2>&1 &  # port 8308
 # TODO: p3-osm-transformer, p3-bing-translate-transformer 
 
 # starts up Elasticsearch - Logstash - Kibana
